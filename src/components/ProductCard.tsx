ed84fc4322aabfe7366be37c9484ff8e3832235e
Husen Badri <badri.hussain323@gmail.com>
2025-04-26 10:21:09 +0530

    fix image issue

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -6,7 +6,7 @@ interface ProductCardProps {
   title: string;
   description?: string;
   slug?: string; // Optional prop for slug
-  price?: string;
+  price?: string;// Optional prop for type
   // price: {
   //   original: string;
   //   discounted?: string;
@@ -14,14 +14,29 @@ interface ProductCardProps {
 }
 
 export default function ProductCard({ image, title, description, slug }: ProductCardProps) {
-  const imgWidth = 200; // Set a fixed width for the image
-  const imgHeight = 200; // Set a fixed height for the image
+  // const imgWidth = type === 'full'? 300: 200; // Set a fixed width for the image
+  // const imgHeight = type === 'full'? 300: 200;; // Set a fixed height for the image
   return (
     <Link href={`/products/${slug}`}>
-    <div className="bg-orange-100 p-3 md:p-6 rounded-xl shadow-md text-center transition-transform duration-300 hover:scale-105 hover:shadow-xl hover:bg-yellow-200">
-      <Image src={image} alt={title} className="h-38 sm:h-45 lg:h-50 mx-auto mb-4" width={imgWidth} height={imgHeight} />
-      <h3 className="text-sm lg:text-lg font-bold mb-2 line-clamp-2 h-10 sm:h-13">{title}</h3>
-      {description && <p className="text-sm text-gray-700">{description}</p>}
+    <div className="bg-orange-100 p-3 mx-4 sm:mx-0 md:p-6 rounded-xl shadow-md text-center transition-transform duration-300 hover:scale-105 hover:shadow-xl hover:bg-yellow-200">
+      {/* <Image src={image} alt={title} 
+      fill
+      className="object-cover rounded-xl"
+      sizes="(max-width: 768px) 100vw, 33vw"
+      priority/> */}
+      
+      {/* // className=" sm:h-45 lg:h-50 mx-auto mb-4" width={imgWidth} height={imgHeight} /> */}
+      <div className="relative w-full aspect-[4/3] overflow-hidden rounded-xl mb-4">
+  <Image
+    src={image}
+    alt={title}
+    fill
+    className="object-cover"
+    sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
+  />
+</div>
+      <h3 className="text-lg font-bold mb-2 line-clamp-2 h-15 sm:h-13">{title}</h3>
+      {description && <p className="text-md text-gray-700 line-clamp-2">{description}</p>}
     </div>
     </Link>
   );
