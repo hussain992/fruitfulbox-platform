4aeb4725025c95961c54b7af1bdd03ef5c603f08
Husen Badri <badri.hussain323@gmail.com>
2025-06-17 17:36:02 +0530

    updated sitemap

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,35 +1,22 @@
 // app/sitemap.xml/route.ts
 
 import { productData } from '../../lib/products'; // adjust as per your path
+import type { MetadataRoute } from 'next'
 
-export async function GET() {
+export async function GET (): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://fruitfulbox.vercel.app'; // Replace with real domain or use env
 
-const staticRoutes = ['', '/products'];
-const productArr = (Object.keys(productData) as Array<keyof typeof productData>).map(key => ({ key: key, value: productData[key] }));
+  const staticRoutes = ['', '/products'];
+  const productArr = (Object.keys(productData) as Array<keyof typeof productData>).map(key => ({ key: key, value: productData[key] }));
   const productRoutes = productArr.map(
     (product) => `/products/${product.value.id}`
   );
 
   const urls = [...staticRoutes, ...productRoutes];
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls
-      .map(
-        (url) => `
-      <url>
-        <loc>${baseUrl}${url}</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-      </url>`
-      )
-      .join('')}
-  </urlset>`;
-
-  return new Response(sitemap, {
-    headers: {
-      'Content-Type': 'application/xml',
-    },
-  });
+  return urls.map((url) => ({
+    url: `${baseUrl}${url}`,
+    changeFrequency: 'weekly',
+    priority: 0.8,
+  }));
 }
