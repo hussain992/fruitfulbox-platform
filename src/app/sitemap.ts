d3bf721b6e710f1fd2b589e89be519a79d144418
Husen Badri <badri.hussain323@gmail.com>
2025-06-19 16:49:05 +0530

    upd site

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,10 +3,11 @@
 import { productData } from '../lib/products'; // adjust as per your path
 import type { MetadataRoute } from 'next'
 
-export default function GET (): MetadataRoute.Sitemap {
+export default async function sitemap (): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://fruitfulbox.vercel.app'; // Replace with real domain or use env
 
   const staticRoutes = ['', '/products'];
+  console.log("Generating sitemap...",productData);
   const productArr = (Object.keys(productData) as Array<keyof typeof productData>).map(key => ({ key: key, value: productData[key] }));
   const productRoutes = productArr.map(
     (product) => `/products/${product.value.id}`
@@ -16,6 +17,7 @@ export default function GET (): MetadataRoute.Sitemap {
 
   return urls.map((url) => ({
     url: `${baseUrl}${url}`,
+    lastModified: new Date(),
     changeFrequency: 'weekly',
     priority: 0.8,
   }));
