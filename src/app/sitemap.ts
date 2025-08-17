316324cd9302b19d4146c2224ed26026301cc106
Husen Badri <badri.hussain323@gmail.com>
2025-08-17 18:24:59 +0530

    sitemap change

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,30 @@
 // app/sitemap.xml/route.ts
 
-import { productData } from '../lib/products'; // adjust as per your path
+import fruits from '@/lib/fruits.json'; 
+import jams from '@/lib/jams.json';
+import cutFruits from '@/lib/cut_fruits.json';
+import boxes from '@/lib/boxes.json'; 
 import type { MetadataRoute } from 'next'
 
 export default async function sitemap (): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://fruitfulbox.vercel.app'; // Replace with real domain or use env
 
-  const staticRoutes = ['', '/products'];
-  const productArr = (Object.keys(productData) as Array<keyof typeof productData>).map(key => ({ key: key, value: productData[key] }));
-  const productRoutes = productArr.map(
-    (product) => `/products/${product.value.id}`
+  const staticRoutes = ['', '/fruits', '/cut_fruits', '/jams', '/boxes'];
+  // const productArr = fruits.map(key => ({ key: key, value: fruits }));
+  const fruitsRoutes = fruits.map(
+    (product) => `/fruits/${product.slug}`
+  );
+  const jamsRoutes = jams.map(
+    (product) => `/jams/${product.slug}`
+  );
+  const cutFruitsRoutes = cutFruits.map(
+    (product) => `/cutFruits/${product.slug}`
+  );
+  const boxesRoutes = boxes.map(
+    (product) => `/boxes/${product.slug}`
   );
 
-  const urls = [...staticRoutes, ...productRoutes];
+  const urls = [...staticRoutes, ...fruitsRoutes, ...jamsRoutes, ...cutFruitsRoutes, ...boxesRoutes];
 
   return urls.map((url) => ({
     url: `${baseUrl}${url}`,
