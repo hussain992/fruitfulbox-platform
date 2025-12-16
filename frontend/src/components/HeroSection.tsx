23d37cd5f0e671e4c8d1c9539e6952b505625117
Husen Badri <badrihussain323@gmail.com>
2025-12-16 18:06:47 +0530

    change banner

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 
 export default function Hero() {
   return (
-    <section className="relative w-full h-[150px] md:h-[400px] lg:h-[600px] xl:h-[350px] 2xl:h-[800px]">
+    <section className="relative w-full h-[250px] md:h-[400px] lg:h-[600px] xl:h-[550px] 2xl:h-[800px]">
       <div className="">
         <Image
-          src="/images/banner.png"
+          src="/images/bannernw.png"
           alt="Pearl Pattern"
           fill
           className=""
