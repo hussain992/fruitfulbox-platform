ec78c139bf89c3f19963a2d3f36708387cddce12
Husen Badri <badrihussain323@gmail.com>
2026-04-03 11:48:21 +0530

    image optimize

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <section className="relative w-full h-[200px] md:h-[400px] lg:h-[600px] xl:h-[550px] 2xl:h-[800px]">
       <div className="">
         <Image
-          src="/images/bannernw.png"
+          src="/images/bannernw.webp"
           alt="Pearl Pattern"
           fill
           className=""
